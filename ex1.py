7be8c06f99bacd4fa70d28cabd3aecc25451af28
Cristiano Nunes <cfgnunes@gmail.com>
2019-03-10 19:18:47 -0300

    Updated ex1.py

--- ex1.py
+++ ex1.py
@@ -1,14 +1,18 @@
 """
+Computer Vision Lab
 Example: Read and show an image.
+
+Author: Cristiano Fraga G. Nunes
+E-mail: <cfgnunes@gmail.com>
 """
 
 import cv2
 
 # Read an image
-img1 = cv2.imread('lena.jpg')
+img1 = cv2.imread("lena.jpg")
 
 # Show the image in a window
-cv2.imshow('Window title', img1)
+cv2.imshow("Window title", img1)
 
 # Pause the application until any key is pressed
 cv2.waitKey(0)
