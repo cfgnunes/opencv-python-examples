dc06171fa2d3ae3c0f488b41da1ac758e8da7901
Cristiano Nunes <cfgnunes@gmail.com>
2019-03-19 20:09:34 -0300

    Added example 07

---
+++ 07_reading_video.py
@@ -0,0 +1,21 @@
+import cv2
+
+cap = cv2.VideoCapture('videos/chaplin.mp4')
+
+# Check if the video is opened correctly
+if not cap.isOpened():
+    raise IOError("Could not open video")
+
+frames_to_read = 120
+frame_list = []
+
+for i in range(frames_to_read):
+    _, frame = cap.read()
+    frame_list.append(frame)
+
+cap.release()
+
+# Show the 10th frame
+cv2.imshow('Frame 10', frame_list[10])
+
+cv2.waitKey()
